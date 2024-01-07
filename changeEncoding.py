25efbd8985cf3e43fe799254541d9b8ab6a1a134
quincy <insta0430@gmail.com>
2024-01-07 13:58:19 +0900

    First commit

---
+++ changeEncoding.py
@@ -0,0 +1,49 @@
+import codecs
+import glob
+
+import chardet
+from tqdm import tqdm
+
+TO_ENCODEING = "utf-8"
+PATH = "./" + "*.txt"
+
+def extentEncoding(encoding):
+    encoding = encoding.lower()
+    if encoding == "euc-kr":
+        return "cp949"
+    else:
+        return encoding
+
+def convert_encoding(file_path):
+    with open(file_path, "rb") as f:
+        r = f.read()
+        findLength = max(len(r), 200)
+        detect = chardet.detect(r[:findLength])
+        encoding = extentEncoding(detect["encoding"])
+
+        if detect["confidence"] <= 0.2:         return -1
+        if encoding.lower() == TO_ENCODEING:    return 0
+
+        try:    content = r.decode(encoding, errors="ignore")
+        except: return -1
+    
+    with codecs.open(file_path, 'w', TO_ENCODEING) as file:
+        file.write(content)
+    
+    return 0
+
+
+
+
+if __name__ == "__main__":
+    file_list = glob.glob(PATH)
+    print("FOUND %d FILE(S)\n"%len(file_list))
+
+    errorFile = []
+    for filePath in tqdm(file_list):
+        if convert_encoding(filePath) == -1:
+            errorFile.append(filePath)
+
+    print("\nEncode Success. %d File Failed"%len(errorFile))
+    for filePath in errorFile:
+        print(filePath.split('/')[-1])
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Change encoding
+
+Change encoding is python tool for change text file encoding.
+
+Code detect original text file encoding automatically. So, you don't have to specify file encoding.
+
+
+
+## Constant value
+
+TO_ENCODING: Result encoding.
+
+PATH: File path.
